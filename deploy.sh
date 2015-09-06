7c1403c73bc408513d017617fb4b89b215d29c00
fatedier <512654112@qq.com>
2015-09-06 16:39:46 +0800

    add deploy.sh for upload myblog to github pages

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+echo "Deploying myblog to github..."
+
+hugo
+
+cp ./cp_to_public/* ./public/
+
+git add -A
+
+if [ $# -eq 1 ]; then
+    git commit -m "$1"
+else
+    git commit
+fi
+
+git push origin master
+
+git subtree push --prefix=public git@github.com:fatedier/just-my-blog.git gh-pages

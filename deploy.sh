668ec066b75ce5b934dbd2045e7a70d940af354f
fatedier <fatedier@gmail.com>
2017-02-25 02:40:55 +0800

    update for google analytics

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-echo "Deploying myblog to github and coding.net..."
+echo "Deploying myblog to github"
 
 # generate static html, css file using hugo
 hugo
 
-# copy some static content to ./public to publish
-cp -rf ./cp_to_public/* ./public/
+# copy some static content to ./docs to publish
+cp -rf ./cp_to_docs/* ./docs/
 
-git add -A
+git add -A :/
 
 if [ $# -eq 1 ]; then
     git commit -m "$1"
@@ -23,11 +23,3 @@ fi
 
 # push to github
 git push origin master
-# now i use coding.net, so comment this line
-git subtree push --prefix=public git@github.com:fatedier/just-my-blog.git gh-pages
-
-# push to coding.net
-git remote add coding git@git.coding.net:fatedier/just-my-blog.git 1> /dev/null 2>&1
-
-git push coding master
-git subtree push --prefix=public git@git.coding.net:fatedier/just-my-blog.git gh-pages
